21013aaad87b2994731371f46e51126ca67c7585
Tomislav Grospic <grospic@gmail.com>
2017-05-20 10:15:55 +0200

    Add emotion detector

--- FamousCroatianConfessionBot.BotApi/Controllers/MessagesController.cs
+++ FamousCroatianConfessionBot.BotApi/Controllers/MessagesController.cs
@@ -1,143 +1,198 @@
-﻿using System.Net;
+﻿using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+using Microsoft.ProjectOxford.Emotion;
+using Microsoft.ProjectOxford.Emotion.Contract;
+using System;
+using System.IO;
+using System.Linq;
+using System.Net;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 using System.Web.Http;
-using Microsoft.Bot.Builder.Dialogs;
-using Microsoft.Bot.Connector;
-using System;
-using System.Text;
 
-namespace FamousCroatianConfessionBot
-{
-	[BotAuthentication]
-	public class MessagesController : ApiController
-	{
-		/// <summary>
-		/// POST: api/Messages
-		/// Receive a message from a user and reply to it
-		/// </summary>
-		[HttpGet, HttpPost]
-		public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
-		{
-			// Global values
-			bool boolAskedForUserName = false;
-			string strUserName = "";
+namespace FamousCroatianConfessionBot {
 
-			if (activity.Type == ActivityTypes.Message)
-			{
-				// Get any saved values
-				StateClient sc = activity.GetStateClient();
-				BotData userData = sc.BotState.GetPrivateConversationData
-				(
-					activity.ChannelId, 
-					activity.Conversation.Id, 
-					activity.From.Id
-				);
+  [BotAuthentication]
+  public class MessagesController : ApiController {
+    
+    #region Types
 
-				boolAskedForUserName = userData.GetProperty<bool>("AskedForUserName");
-				strUserName = userData.GetProperty<string>("UserName") ?? "";
+    class UData {
+      public string UserName { get; set; }
+      public bool AskedForUserName { get; set; }
+    }
 
-				// Create text for a reply message   
-				StringBuilder strReplyMessage = new StringBuilder();
+    class State : IDisposable {
+      Activity _activity = null;
+      StateClient _sc = null;
+      BotData _botData = null;
+      public State( Activity activity ) {
+        // Get any saved values
+        _activity = activity;
+        _sc = activity.GetStateClient();
+        _botData = _sc.BotState.GetPrivateConversationData
+        (
+          activity.ChannelId,
+          activity.Conversation.Id,
+          activity.From.Id
+        );
+      }
 
-				if (boolAskedForUserName == false) // Never asked for name
-				{
-					strReplyMessage.Append($"Hello, I am **FCCBot**");
-					strReplyMessage.Append($"\n");
-					strReplyMessage.Append($"You can say anything");
-					strReplyMessage.Append($"\n");
-					strReplyMessage.Append($"to me and I will repeat it back");
-					strReplyMessage.Append($"\n\n");
-					strReplyMessage.Append($"What is your name?");
+      public UData Get() => _botData.GetProperty<UData>( nameof( UData ) ) ?? new UData();
 
-					// Set BotUserData
-					userData.SetProperty<bool>("AskedForUserName", true);
-				}
-				else // Have asked for name
-				{
-					if (strUserName == "") // Name was never provided
-					{
-						// If we have asked for a username but it has not been set
-						// the current response is the user name
-						strReplyMessage.Append($"Hello {activity.Text}!");
+      public void Set( UData data ) {
+        _botData.SetProperty( nameof( UData ), data );
+        _sc.BotState.SetPrivateConversationData
+        (
+          _activity.ChannelId,
+          _activity.Conversation.Id,
+          _activity.From.Id, _botData
+        );
+      }
 
-						// Set BotUserData
-						userData.SetProperty<string>("UserName", activity.Text);
-					}
-					else // Name was provided
-					{
-						strReplyMessage.Append($"{strUserName}, You said: {activity.Text}");
-					}
-				}
-				// Save BotUserData
-				sc.BotState.SetPrivateConversationData
-				(
-					activity.ChannelId, 
-					activity.Conversation.Id, 
-					activity.From.Id, userData
-				);
+      public void Dispose() {
+        _sc.Dispose();
+      }
+    }
 
-				// Create a reply message
-				ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl));
-				Activity replyMessage = activity.CreateReply(strReplyMessage.ToString());
+    string MsCognitiveApiKey = System.Web.Configuration.WebConfigurationManager.AppSettings["MsCognitiveApiKey"];
 
-				await connector.Conversations.ReplyToActivityAsync(replyMessage);
-			}
-			else
-			{
-				Activity replyMessage = HandleSystemMessage(activity);
-				if (replyMessage != null)
-				{
-					ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl));
-					await connector.Conversations.ReplyToActivityAsync(replyMessage);
-				}
-			}
+    #endregion
 
-			// Return response
-			var response = Request.CreateResponse(HttpStatusCode.OK);
-			return response;
-		}
+    string RespondOnText( UData data, Activity activity ) {
+      StringBuilder strReplyMessage = new StringBuilder();
+      if ( data.AskedForUserName == false ) {
+        strReplyMessage.Append( $"Hej! Šta ima!" );
+        strReplyMessage.Append( $"\n\r" );
+        strReplyMessage.Append( $"Šta god da kažeš, koga zanima?" );
 
-		private Activity HandleSystemMessage(Activity message)
-		{
-			if (message.Type == ActivityTypes.DeleteUserData)
-			{
-				// Get BotUserData
-				StateClient sc = message.GetStateClient();
-				BotData userData = sc.BotState.GetPrivateConversationData(
-						message.ChannelId, message.Conversation.Id, message.From.Id);
-				// Set BotUserData
-				userData.SetProperty<string>("UserName", "");
-				userData.SetProperty<bool>("AskedForUserName", false);
-				// Save BotUserData
-				sc.BotState.SetPrivateConversationData(
-						message.ChannelId, message.Conversation.Id, message.From.Id, userData);
-				// Create a reply message
-				ConnectorClient connector = new ConnectorClient(new Uri(message.ServiceUrl));
-				Activity replyMessage = message.CreateReply("Personal data has been deleted.");
+        // Set BotUserData
+        data.AskedForUserName = true;
+      } else {
+        if ( data.UserName == null ) // Name was never provided
+        {
+          // If we have asked for a username but it has not been set
+          // the current response is the user name
+          strReplyMessage.Append( $"Hello {activity.Text}!" );
 
-				return replyMessage;
-			}
-			else if (message.Type == ActivityTypes.ConversationUpdate)
-			{
-				// Handle conversation state changes, like members being added and removed
-				// Use Activity.MembersAdded and Activity.MembersRemoved and Activity.Action for info
-				// Not available in all channels
-			}
-			else if (message.Type == ActivityTypes.ContactRelationUpdate)
-			{
-				// Handle add/remove from contact lists
-				// Activity.From + Activity.Action represent what happened
-			}
-			else if (message.Type == ActivityTypes.Typing)
-			{
-				// Handle knowing tha the user is typing
-			}
-			else if (message.Type == ActivityTypes.Ping)
-			{
-			}
+          // Set BotUserData
+          //data.UserName = activity.Text;
+        } else // Name was provided
+          {
+          strReplyMessage.Append( $"{data.UserName}, You said: {activity.Text}" );
+        }
+      }
+      return strReplyMessage.ToString();
+    }
 
-			return null;
-		}
-	}
+    async Task<string> RespondOnImage( Attachment att ) {
+      var isImage = att.ContentType.Contains( "image" );
+      if ( isImage ) {
+        var emotionServiceClient = new EmotionServiceClient( MsCognitiveApiKey );
+        try {
+          var request = HttpWebRequest.Create( att.ContentUrl );
+          var response = request.GetResponse();
+          var imgStream = response.GetResponseStream();
+          var emotions = await emotionServiceClient.RecognizeAsync( imgStream );
+          var strEmotions = emotions.Select( PrintEmotion );
+          return string.Join( "\n\r\n\r", strEmotions );
+        } catch ( Exception ex ) {
+          return ex.Message;
+        }
+      }
+      return null;
+    }
+
+    string PrintEmotion( Emotion e ) {
+      var s = new[] {
+        $" ˇˇˇˇˇˇˇ SCORE ˇˇˇˇˇˇˇ",
+        $"Anger:     {PrintFloat(e.Scores.Anger)}",
+        $"Contempt:  {PrintFloat(e.Scores.Contempt)}",
+        $"Disgust:   {PrintFloat(e.Scores.Disgust)}",
+        $"Fear:      {PrintFloat(e.Scores.Fear)}",
+        $"Happiness: {PrintFloat(e.Scores.Happiness)}",
+        $"Neutral:   {PrintFloat(e.Scores.Neutral)}",
+        $"Sadness:   {PrintFloat(e.Scores.Sadness)}",
+        $"Surprise:  {PrintFloat(e.Scores.Surprise)}"
+      };
+      return System.String.Join( "\n\r", s );
+    }
+
+    string PrintFloat( float f ) => f.ToString( "0.########" );
+
+    /// <summary>
+    /// POST: api/Messages
+    /// Receive a message from a user and reply to it
+    /// </summary>
+    [HttpGet, HttpPost]
+    public async Task<HttpResponseMessage> Post( [FromBody]Activity activity ) {
+      // Global values
+      if ( activity.Type == ActivityTypes.Message ) {
+        var state = new State( activity );
+        var data = state.Get();
+        string text = null;
+
+        if ( activity.Attachments.Any() ) {
+          // Process attachments
+          text = await RespondOnImage( activity.Attachments[0] );
+        }
+        
+        if ( text == null ) {
+          // Create text for a reply message   
+          text = RespondOnText( data, activity );
+        }
+
+        // Save BotUserData
+        state.Set( data );
+
+        // Create a reply message
+        ConnectorClient connector = new ConnectorClient( new Uri( activity.ServiceUrl ) );
+        Activity replyMessage = activity.CreateReply( text );
+
+        await connector.Conversations.ReplyToActivityAsync( replyMessage );
+      } else {
+        Activity replyMessage = HandleSystemMessage( activity );
+        if ( replyMessage != null ) {
+          ConnectorClient connector = new ConnectorClient( new Uri( activity.ServiceUrl ) );
+          await connector.Conversations.ReplyToActivityAsync( replyMessage );
+        }
+      }
+
+      // Return response
+      var response = Request.CreateResponse( HttpStatusCode.OK );
+      return response;
+    }
+
+    Activity HandleSystemMessage( Activity message ) {
+      if ( message.Type == ActivityTypes.DeleteUserData ) {
+        // Get BotUserData
+        StateClient sc = message.GetStateClient();
+        BotData userData = sc.BotState.GetPrivateConversationData(
+            message.ChannelId, message.Conversation.Id, message.From.Id );
+        // Set BotUserData
+        userData.SetProperty( nameof( UData ), new UData() );
+        // Save BotUserData
+        sc.BotState.SetPrivateConversationData(
+            message.ChannelId, message.Conversation.Id, message.From.Id, userData );
+        // Create a reply message
+        ConnectorClient connector = new ConnectorClient( new Uri( message.ServiceUrl ) );
+        Activity replyMessage = message.CreateReply( "Personal data has been deleted." );
+
+        return replyMessage;
+      } else if ( message.Type == ActivityTypes.ConversationUpdate ) {
+        // Handle conversation state changes, like members being added and removed
+        // Use Activity.MembersAdded and Activity.MembersRemoved and Activity.Action for info
+        // Not available in all channels
+      } else if ( message.Type == ActivityTypes.ContactRelationUpdate ) {
+        // Handle add/remove from contact lists
+        // Activity.From + Activity.Action represent what happened
+      } else if ( message.Type == ActivityTypes.Typing ) {
+        // Handle knowing tha the user is typing
+      } else if ( message.Type == ActivityTypes.Ping ) {
+      }
+
+      return null;
+    }
+  }
 }
